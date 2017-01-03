ec38bf919889e0732d1bf7dae8d6a6b159507e1e
Shaed Ali <shaed.ali@gmail.com>
2017-01-04 00:16:44 +0530

    StringBuffer example added

---
+++ StringBufferEx.java
@@ -0,0 +1,12 @@
+class StringBufferEx 
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer sb1=new StringBuffer("Durga");
+		StringBuffer sb2=sb1.append(" Software");
+		StringBuffer sb3=sb2.append(" Solutions");
+		System.out.println(sb1);
+		System.out.println(sb2);
+		System.out.println(sb3);
+	}
+}
