dca599186b49c1d451ac2ffdc102ac0b13ca090f
Shaed Ali <shaed.ali@gmail.com>
2017-01-05 23:01:26 +0530

    test26 program added

---
+++ test26.java
@@ -0,0 +1,40 @@
+class test26 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Before Loop");
+		for(int i=0;i<=0||i>=0;i++)
+		{
+			System.out.println("Inside Loop");
+		}
+	
+		System.out.println("Aftre Loop");
+	}
+}
+/*class test26 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Before Loop");
+		for(int i=0;true;i++)
+		{
+			System.out.println("Inside Loop");
+		}
+	
+		System.out.println("Aftre Loop");
+	}
+}
+class test26 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Before Loop");
+		for(int i=0;;i++)
+		{
+			System.out.println("Inside Loop");
+		}
+	
+		System.out.println("Aftre Loop");
+	}
+}*/
+
